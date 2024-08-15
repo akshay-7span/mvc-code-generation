0b1cd191b64d38ee1b9701f0be46c93a4f9b07e9
Krupalee-7span <Krupalee@7Span.com>
2024-08-15 14:09:58 +0530

    Initial commit

---
+++ src/test/java/com/mvc_code_generation/MvcCodeGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc_code_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCodeGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
